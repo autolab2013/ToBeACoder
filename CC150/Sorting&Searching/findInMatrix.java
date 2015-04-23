a7cbd0a5906a7abcb8400e9673b768b29fc3b1e8
Yunong Liu <yl2494@cornell.edu>
2015-04-23 10:50:51 -0400

    getLargestPeopleTower more than 1 way?
    getLargestPeopleInTower is get longest increasing sequeces after sort?

--- CC150/Sorting&Searching/findInMatrix.java
+++ CC150/Sorting&Searching/findInMatrix.java
@@ -8,7 +8,19 @@ class Coord{
 	public int y;
 } 
 
+//basic idea:
+//start with right most corner:
+//	if rightmost > x, search in left col 
+// 	if < x, search next row
+//	while row < M and col >= 0
 public Coord findInMatrix(int[][] mat, int M, int N, int element){
-	Coord result = null;
-	return result;
+	//rightmost
+	int row = 0;
+	int col = N-1;
+	while(row < M-1 && col >=0 ){
+		if(mat[row][col] == element) return new Coord(row, col);
+		if(mat[row][col] > element) col--;
+		else row++;
+	}return null;
 } 
+
